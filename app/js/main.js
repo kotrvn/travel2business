634d25cfddb37bda60a24cc81045947f8eeb13b8
Konstantin Travin <travin.kos@yandex.ru>
2018-03-25 18:42:44 +0300

    Разметка и стилизация

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,6 @@
-$(document).ready(function(){ 
-  $(function(){
-    $(window).scroll(function() {
-      if($(this).scrollTop() >= 350) {
-        $('.js-sticky').addClass('sticked-header');
-        }
-        else {
-      $('.js-sticky').removeClass('sticked-header');
-      }
+$(document).ready(function() {
+    $('#fullpage').fullpage( {
+        menu: '#menu',
+        navigation: false
     });
-  });
 });
